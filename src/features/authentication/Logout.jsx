d8ececa54e040546b7b5a6d663f95102b293b799
Giriraj Joshi <levilightsg@gmail.com>
2026-01-16 13:50:16 +0530

    Chage useLOgout to useLogout in authentication/Logout.jsx

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ButtonIcon from "../../ui/ButtonIcon";
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
-import { useLogout } from "./useLOgout";
+import { useLogout } from "./useLogout";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 const Logout = () => {
